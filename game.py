383ff906bcb292e3634404ba2e03d5814e5ed80b
Sandy <sandychen123456789@gmail.com>
2023-12-26 21:42:44 +0800

    新增題目，猜對結束

--- game.py
+++ game.py
@@ -1,5 +1,15 @@
 import random
 #1~50
+#玩家猜5次
+#猜對提前離開
+#猜錯提示答案
+quest=random.randint(1,50)
+print(quest)
 for i in range(5):
-    x=random.randint(1,50)
-    print(x)
+    ans=int(input('guess number(1~50)'))
+    if ans==quest:
+        print('bingo!') 
+        break
+    else:
+        print('wrong number~')   
+    
