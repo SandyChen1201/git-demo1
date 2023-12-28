8f717b6acdc53f75130dfde38649ea4f35f1fff3
Sandy <sandychen123456789@gmail.com>
2023-12-28 19:03:12 +0800

    modify game to complete...version 2!

--- game.py
+++ game.py
@@ -1,15 +1,18 @@
 import random
-#1~50
-#玩家猜5次
-#猜對提前離開
-#猜錯提示答案
-quest=random.randint(1,50)
-print(quest)
+
+# 1~50
+# 玩家猜5次
+# 猜對提前離開
+# 猜錯提示答案
+quest = random.randint(1, 50)
+# print(quest)
 for i in range(5):
-    ans=int(input('guess number(1~50)'))
-    if ans==quest:
-        print('bingo!') 
+    ans = int(input("guess number(1~50)"))
+    if ans == quest:
+        print("bingo!")
         break
     else:
-        print('wrong number~')   
-    
+        if ans > quest:
+            print("wrong number~,lower~")
+        else:
+            print("wrong number~higher~")
